0bb3e2d31039067cb9ce699cf17135075b1e8a51
Marcd <studio@marcd.co>
2026-03-06 10:37:12 -0500

    ud

--- components/layout.js
+++ components/layout.js
@@ -71,25 +71,31 @@ const Layout = ({ site = {}, page = {}, schema, children }) => {
     }
   }, [width])
 
+  const gtmId = site.gtmID || 'GTM-NB3XS74'
+
   return (
     <>
       <HeadSEO site={site} page={page} schema={schema} />
-      {site.gtmID && (
+      {gtmId && (
         <>
           <Script
-            strategy="afterInteractive"
-            src={`https://www.googletagmanager.com/gtag/js?id=${site.gtmID}`}
-          />
-          <Script
+            id="gtm-script"
             strategy="afterInteractive"
             dangerouslySetInnerHTML={{
               __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-${site.gtmID}', {
-              page_path: window.location.pathname,
-            });
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${gtmId}');
+          `,
+            }}
+          />
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `
+            <iframe src="https://www.googletagmanager.com/ns.html?id=${gtmId}"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>
           `,
             }}
           />
